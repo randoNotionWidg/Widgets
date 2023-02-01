98cc83bbcd95e8c3732b508f787e407c1daa6c82
randoNotionWidg <124201076+randoNotionWidg@users.noreply.github.com>
2023-02-01 12:09:59 -0800

    Delete bill-widget.js

--- bill-widget.js
+++
@@ -1,9 +0,0 @@
-  <div id="ajaxContent"></div>
-  <script>
-  var Webflow = Webflow || [];
-  Webflow.push(function() {
-    $.get('https://cdn.rawgit.githubusercontent.com/randoNotionWidg/Widgets/main/bill-widget.html?token=GHSAT0AAAAAAB6HHIYCW7VL43PI3BUPPZMAY62XUUQ', function(data) {
-      $('#ajaxContent').append(data);
-    });
-  });
-  </script>
